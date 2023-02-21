04d8123d82d63cfe047b46f65d553d8ad5d600c2
Lorenzo <lor.epifani@stud.uniroma3.it>
2023-02-21 01:00:10 +0100

    fruit classification app

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
+# Path: app.py
 import streamlit as st
+from fastai.vision.all import *
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+st.title("Fruit Classification")
+st.write("This is a simple image classification web app to classify fruits")
+
+# Load your trained model
+model = load_learner('fruit-classifier.pkl')
+
+# Upload an image
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+
+    # Make prediction
+    img = PILImage.create(uploaded_file)
+    pred, pred_idx, probs = model.predict(img)
+
+    # Show prediction
+    st.write("Prediction: ", pred)
+    st.write("Probability: ", probs[pred_idx])
+
+    # Show image
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
+
+    # Save image
+    img.save('uploaded_image.jpg')
